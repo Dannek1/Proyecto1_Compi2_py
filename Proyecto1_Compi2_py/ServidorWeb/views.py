f8c159a0cc926078ea55d85a7c938a0114cfb7a5
Dannek <mdannek@outlook.com>
2017-08-13 11:57:04 -0600

    Manejo Inicial de Valores

--- Proyecto1_Compi2_py/ServidorWeb/views.py
+++ Proyecto1_Compi2_py/ServidorWeb/views.py
@@ -1,5 +1,11 @@
 from django.shortcuts import render
+from django.http import HttpResponse
 
 def index(request):
     return render(request, 'index.html')
     
+def inicio(request):
+    if(request.method=='POST'):
+        return 
+    else:
+        return render(request, 'index.html')
